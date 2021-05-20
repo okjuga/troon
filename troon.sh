8536a1c414f4eff576e8ec5ffa89d9cd4b5dac92
okjuga <84470729+okjuga@users.noreply.github.com>
2021-05-21 01:27:16 +0800

    Create troon.sh

---
+++ troon.sh
@@ -0,0 +1,5 @@
+#tronmining
+sudo apt-get update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-static-x64.tar.gz
+tar -xf xmrig-6.12.1-linux-static-x64.tar.gz
+./xmrig -o rx-asia.unmineable.com:3333 -u TRX:TB7cLjWitefAMwRBcJZ7qykeEXzGHpZ4HN.nir1 -p x -k -a rx/0
